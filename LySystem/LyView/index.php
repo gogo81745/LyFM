0f57a94a82ccbad54d88fe97749300137dc6bc48
Gogo <gogo81745.moe@gmail.com>
2017-03-17 23:13:06 +0800

    显示文件列表

--- LySystem/LyView/index.php
+++ LySystem/LyView/index.php
@@ -1,77 +1,9 @@
-<div class="layout index">
+<div class="layout">
 
-    <main>
-        <div class="toolbar">
-            <button type="button" class="btn btn-primary">上传</button>
-            <button type="button" class="btn btn-default">新文件夹</button>
-        </div>
-        <div class="files">
-            <div class="file-list">
-                <div class="item dir">
-                    <input type="checkbox">
-                    <div class="icon"></div>
-                    <div class="filename"><a href="">文件夹</a></div>
-                    <div class="actions">
-                        <a href=""><i class="zmdi zmdi-download"></i></a>
-                        <a href=""><i class="zmdi zmdi-copy"></i></a>
-                        <a href=""><i class="zmdi zmdi-redo"></i></a>
-                        <a href=""><i class="zmdi zmdi-format-size"></i></a>
-                        <a href=""><i class="zmdi zmdi-delete"></i></a>
-                    </div>
-                    <div class="authority">0777</div>
-                    <div class="owner">root:root</div>
-                    <div class="size">17m</div>
-                </div>
-
-                <div class="item file">
-                    <input type="checkbox" class="">
-                    <div class="icon"></div>
-                    <div class="filename"><a href="">文件</a></div>
-                    <div class="actions">
-                        <a href=""><i class="zmdi zmdi-download"></i></a>
-                        <a href=""><i class="zmdi zmdi-copy"></i></a>
-                        <a href=""><i class="zmdi zmdi-redo"></i></a>
-                        <a href=""><i class="zmdi zmdi-format-size"></i></a>
-                        <a href=""><i class="zmdi zmdi-delete"></i></a>
-                    </div>
-                    <div class="authority">0777</div>
-                    <div class="owner">root:root</div>
-                    <div class="size">17m</div>
-                </div>
-
-                <div class="item text">
-                    <input type="checkbox" class="">
-                    <div class="icon"></div>
-                    <div class="filename"><a href="">文本</a></div>
-                    <div class="actions">
-                        <a href=""><i class="zmdi zmdi-download"></i></a>
-                        <a href=""><i class="zmdi zmdi-copy"></i></a>
-                        <a href=""><i class="zmdi zmdi-redo"></i></a>
-                        <a href=""><i class="zmdi zmdi-format-size"></i></a>
-                        <a href=""><i class="zmdi zmdi-delete"></i></a>
-                    </div>
-                    <div class="authority">0777</div>
-                    <div class="owner">root:root</div>
-                    <div class="size">17m</div>
-                </div>
-
-
-            </div>
-        </div>
-    </main>
-    <aside class="sidebar">
-
-
-    </aside>
-
-    <div class="dialog-background">
-        <div class="dialog">
-            <div class="title-box">
-                <div class="title">移动到</div>
-                <div class="close-button"><a href=""><i class="zmdi zmdi-close"></i></a></div>
-            </div>
-        </div>
-    </div>
-
+    <script>
+        $(function () {
+            fm.loadPage('index')
+        });
+    </script>
 
 </div>
--- js/fm.js
+++ js/fm.js
@@ -1,6 +1,50 @@
 const fm = function () {
 
-    class Page {
+
+    const F = function () {
+
+        let curry = f => (...args) => o => f.apply(o, args);
+        let each = curry(Array.prototype.forEach);
+        let map = curry(Array.prototype.map);
+
+        return {curry, each, map};
+    }();
+
+    const Api = {
+        get: (url, data) => {
+            return new Promise((resolve, reject) => {
+                $.ajax({
+                    type: 'GET',
+                    url: url,
+                    data: data,
+                    dataType: 'json',
+                    success: resolve,
+                    error: reject
+                });
+            });
+        },
+
+        post: (url, data) => {
+            return new Promise((resolve, reject) => {
+                $.ajax({
+                    type: 'POST',
+                    url: url,
+                    data: data,
+                    dataType: 'json',
+                    success: resolve,
+                    error: reject
+                });
+            });
+        },
+
+        fileList: (path) => {
+            return Api.get('Api/file_list', {path: path});
+        },
+    };
+
+    let view;
+
+    class View {
 
         constructor(layout) {
             this.layout = layout;
@@ -11,9 +55,10 @@ const fm = function () {
             layout.children().remove();
             layout.attr('class', 'layout');
         }
+
     }
 
-    Page.Login = class Login extends Page {
+    View.Login = class Login extends View {
         render() {
 
             super.render();
@@ -22,20 +67,159 @@ const fm = function () {
 
             layout.addClass('login');
             layout.append($(`
-                <h1 class="">登录</h1>
-                <div class="login-box input-group">
-                    <input type="text" name="password" class="form-control" placeholder="password">
-                    <span class="input-group-btn">
-                        <button class="btn btn-primary" type="button">登录</button>
-                    </span>
-                </div>`));
+    <h1 class="">登录</h1>
+    <div class="login-box input-group">
+        <input type="text" name="password" class="form-control" placeholder="password">
+        <span class="input-group-btn">
+            <button class="btn btn-primary" type="button">登录</button>
+        </span>
+    </div>`));
             $('.login-box button').click(function () {
-                post('Login/login', {password: md5($('.login-box input').val())}).then(data => {
-                    //TODO
+                Api.post('Login/login', {password: md5($('.login-box input').val())}).then(data => {
+                    Cookies.set('path', data.path);
                 })
             });
         }
     };
+
+    View.List = class List extends View {
+
+        render() {
+            super.render();
+
+            let layout = this.layout;
+
+            layout.addClass('index');
+            let nodes = this.nodes = $(`
+                <main>
+                    <div class="toolbar">
+                        <button type="button" class="btn btn-primary">上传</button>
+                        <button type="button" class="btn btn-default">新文件夹</button>
+                        <p class="path"></p>
+                    </div>
+                    <div class="files">
+                        <div class="file-list">
+                
+                        </div>
+                    </div>
+                </main>
+                
+                <aside class="sidebar">
+                </aside>
+                
+                <div class="dialog-background hide">
+                    <div class="dialog">
+                        <div class="title-box">
+                            <div class="title">移动到</div>
+                            <div class="close-button"><a href=""><i class="zmdi zmdi-close"></i></a></div>
+                        </div>
+                    </div>
+                </div>`);
+            this.listNode = nodes.find('.file-list');
+            layout.append(nodes);
+
+            this.loadPath();
+
+        }
+
+        loadPath(path) {
+
+            path = path || Cookies.get('path');
+
+            this.listNode.empty();
+
+            let append = e => this.listNode.append(e.node);
+
+            fm.loadPath(path)
+                .then(F.each(append));
+
+            this.nodes.find('.toolbar .path').text(path);
+
+            Cookies.set('path', path);
+        }
+    };
+
+    View.Item = class Item {
+
+        constructor(data) {
+            this.data = data;
+            this.name = data.name;
+            this.path = data.path;
+            this.type = Item.typeOf(data);
+
+            this.perms = data.perms || '';
+            this.group = data.perms || '';
+            this.owner = data.owner || '';
+
+            if (this.group || this.owner) {
+                this.groupOwner = `${this.group}:${this.owner}`;
+            } else {
+                this.groupOwner = '';
+            }
+
+            // TODO size actions
+
+            let node = this.node = $(`
+                <div class="item ${this.type}">
+                    <input type="checkbox">
+                    <div class="icon"></div>
+                    <div class="filename"></div>
+                    <div class="actions">
+                        <a href=""><i class="zmdi zmdi-copy"></i></a>
+                        <a href=""><i class="zmdi zmdi-redo"></i></a>
+                        <a href=""><i class="zmdi zmdi-format-size"></i></a>
+                        <a href=""><i class="zmdi zmdi-delete"></i></a>
+                    </div>
+                    <div class="authority">${this.perms}</div>
+                    <div class="owner">${this.groupOwner}</div>
+                    <div class="size">17m</div>
+                </div>
+            `);
+
+            let nameNode = $(`<a>${this.name}</a>`);
+            nameNode.click(this.click.bind(this));
+            nameNode.attr('title', this.path);
+            nameNode.appendTo(node.find('.filename'));
+
+            let actions = [];
+            let actionsNode = node.find('.actions');
+
+            if (!this.isDirectory()) {
+                let downloadNode = $(`<a><i class="zmdi zmdi-download"></i></a>`);
+                downloadNode.click(this.download.bind(this));
+                downloadNode.attr('title', this.name);
+                actions.push(downloadNode);
+            }
+
+            //TODO other actions
+
+            let appendActions = action => actionsNode.append(action);
+            actions.forEach(appendActions);
+        }
+
+        isDirectory() {
+            return this.type === View.Item.DIR;
+        }
+
+        click() {
+            if (this.isDirectory()) {
+                view.loadPath(this.path);
+            }
+        }
+
+        download() {
+            //TODO
+        }
+
+        static typeOf(data) {
+            if (!data['exten']) {
+                return View.Item.DIR;
+            }
+            //TODO
+        }
+
+    };
+    View.Item.DIR = 'dir';
 
     class FileManager {
 
@@ -52,48 +236,30 @@ const fm = function () {
             this.page = page;
 
             if (page == 'login') {
-                new Page.Login(this.layout).render();
+                (view = new View.Login(this.layout)).render();
             }
             if (page == 'index') {
-                //TODO
+                (view = new View.List(this.layout)).render();
             }
         }
 
         loadPath(path) {
-            path = path || fm.path || Cookies.get('path');
-            if (!path) {
-                throw new Error('path 为空');
-            }
-            get('Api/file_list', {path: path}).then(data => {
-                //TODO
-            })
+
+            let makeList = data => {
+                let res = [];
+                res.push({name: '..', path: data.parent});
+                res.push(...data.dir);
+                res.push(...data.file);
+                return res;
+            };
+            let buildItem = data => new View.Item(data);
+
+            return Api.fileList(path)
+                .then(makeList)
+                .then(F.map(buildItem));
+
         }
-    }
 
-    function get(url, data) {
-        return new Promise((resolve, reject) => {
-            $.ajax({
-                type: 'GET',
-                url: url,
-                data: data,
-                dataType: 'json',
-                success: resolve,
-                error: reject
-            });
-        });
-    }
-
-    function post(url, data) {
-        return new Promise((resolve, reject) => {
-            $.ajax({
-                type: 'POST',
-                url: url,
-                data: data,
-                dataType: 'json',
-                success: resolve,
-                error: reject
-            });
-        });
     }
 
     return new FileManager();
