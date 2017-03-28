044a3109fc0f89872f00657649d6ced7a3283c7c
Gogo <gogo81745.moe@gmail.com>
2017-03-29 00:16:08 +0800

    目录树后台功能实现

--- config.php
+++ config.php
@@ -3,6 +3,7 @@
 $config = array(
 	'title' => 'LY PHP 文件管理器',
 	'password' => '94a5f0635f5e7163fc23346870d55b52',//default 94a5f0635f5e7163fc23346870d55b52 使用密码的MD5值
+    'root_dir' => '../..',
 	'system' => array(
 		'404_page' => '',//404页面，留空默认系统
 		'cookie_prefix' => 'fm_',//COOKIE前缀,留空为空
